2ebab144dc1e6d1a71f576f72cb8c05129b226bf
Pavankalyany <60839440+Pavankalyany@users.noreply.github.com>
2020-08-23 14:05:21 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,11 @@
+list1=[12,-7,5,64,-14]
+print("output of list 1")
+for numbers in list1:
+    if numbers>=0:
+        print( numbers)
+        
+list2=[12,14,-95,3]
+print("output of list 2")
+for num in list2:
+    if num>=0:
+        print(num)
